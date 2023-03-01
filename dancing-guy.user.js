26419112c5b581667aa71447d464c995075b1ae1
JR Shampang <jr@jr.codes>
2023-03-01 11:03:02 -0600

    Update meta data, reformat

--- dancing-guy.user.js
+++ dancing-guy.user.js
@@ -1,19 +1,23 @@
 // ==UserScript==
-// @name         Dancing Guy
-// @namespace   http://openuserjs.org/users/zarjay/scripts
-// @author       zarjay
-// @version      1.0.0
-// @description  Make a dancing guy appear on every page
-// @match        http://*/*
-// @match        https://*/*
-// @license     MIT License; https://github.com/zarjay/userscripts/blob/master/LICENSE
+// @name        Dancing Guy
+// @namespace	  https://jr.codes
+// @author      JR
+// @version     1.0.1
+// @description Make a dancing guy appear on every page
+// @match       http://*/*
+// @match       https://*/*
+// @license     MIT License; https://github.com/jr-codes/userscripts/blob/main/LICENSE
 // ==/UserScript==
 
 function init() {
-    var src = '//simpl.info/videoalpha/video/dancer1.webm';
-    var style = 'position: fixed; bottom: 0; right: 0; pointer-events: none; background: transparent; z-index: 100';
+  var src = '//simpl.info/videoalpha/video/dancer1.webm'
+  var style =
+    'position: fixed; bottom: 0; right: 0; pointer-events: none; background: transparent; z-index: 100'
 
-    document.body.insertAdjacentHTML('beforeend', '<video src="' + src + '" autoplay loop style="' + style + '"></video>');
+  document.body.insertAdjacentHTML(
+    'beforeend',
+    '<video src="' + src + '" autoplay loop style="' + style + '"></video>'
+  )
 }
 
-init();
+init()
--- jenkins-console-colors.user.js
+++ jenkins-console-colors.user.js
@@ -1,114 +1,105 @@
 // ==UserScript==
 // @name        Jenkins Console Colors
-// @namespace   http://openuserjs.org/users/zarjay/scripts
-// @author      zarjay
-// @version     2.0.0
+// @namespace   http://jr.codes
+// @author      JR
+// @version     2.0.1
 // @description Adds colors to the Jenkins console output
 // @match       http://*/*/job/*/console*
 // @match       https://*/*/job/*/console*
-// @license     MIT License; https://github.com/zarjay/userscripts/blob/master/LICENSE
-// @updateURL   https://openuserjs.org/meta/zarjay/Jenkins_Console_Colors.meta.js
+// @license     MIT License; https://github.com/jr-codes/userscripts/blob/main/LICENSE
 // ==/UserScript==
 
 function css(content) {
-    const element = document.createElement('style');
-    element.textContent = content;
-    document.head.appendChild(element);
+  const element = document.createElement('style')
+  element.textContent = content
+  document.head.appendChild(element)
 }
 
 function getType(line) {
-    const markers = {
-        error: [
-            /\d Error/,
-            /FAILED/,
-            / error /,
-            /^ERROR:/,
-            /^Failed/,
-            /[^0] failed/,
-            /Finished: FAILURE/
-        ],
+  const markers = {
+    error: [
+      /\d Error/,
+      /FAILED/,
+      / error /,
+      /^ERROR:/,
+      /^Failed/,
+      /[^0] failed/,
+      /Finished: FAILURE/,
+    ],
 
-        warning: [
-            /\d Warning/,
-            /\[SKIP]/
-        ],
+    warning: [/\d Warning/, /\[SKIP]/],
 
-        success: [
-            /^Passed/,
-            / 0 failed/,
-            /Build succeeded/,
-            /Finished: SUCCESS/
-        ],
+    success: [/^Passed/, / 0 failed/, /Build succeeded/, /Finished: SUCCESS/],
 
-        important: [
-            /xUnit\.net console test runner/,
-            /Test Execution Command Line Tool/,
-            /Build Engine/,
-            / 0 (Warning|Error)/,
-            /^Build started/
-        ],
+    important: [
+      /xUnit\.net console test runner/,
+      /Test Execution Command Line Tool/,
+      /Build Engine/,
+      / 0 (Warning|Error)/,
+      /^Build started/,
+    ],
 
-        normal: [
-            /^\s*\d+&gt;/
-        ]
-    };
+    normal: [/^\s*\d+&gt;/],
+  }
 
-    for (const type in markers) {
-        for (const pattern of markers[type]) {
-            if (pattern.test(line)) {
-                return type;
-            }
-        }
+  for (const type in markers) {
+    for (const pattern of markers[type]) {
+      if (pattern.test(line)) {
+        return type
+      }
     }
+  }
 
-    return 'minor';
+  return 'minor'
 }
 
 function include(url) {
-    return new Promise((resolve, reject) => {
-        const element = document.createElement('script');
-        element.src = url;
-        element.async = false;
-        element.onload = resolve;
-        element.onerror = reject;
-        document.head.appendChild(element);
-    });
+  return new Promise((resolve, reject) => {
+    const element = document.createElement('script')
+    element.src = url
+    element.async = false
+    element.onload = resolve
+    element.onerror = reject
+    document.head.appendChild(element)
+  })
 }
 
 function parseBlock(block) {
-    console.log('Processing text block', block);
-    block.classList.add('parsed');
+  console.log('Processing text block', block)
+  block.classList.add('parsed')
 
-    const lines = block.innerHTML.split('\n');
-    const html = [];
-    let currentLine = 0;
+  const lines = block.innerHTML.split('\n')
+  const html = []
+  let currentLine = 0
 
-    (function parseChunk() {
-        for (let i = 0; i < 20; ++i, ++currentLine) {
-            if (currentLine >= lines.length) {
-                block.innerHTML = html.join('');
-                return;
-            }
+  ;(function parseChunk() {
+    for (let i = 0; i < 20; ++i, ++currentLine) {
+      if (currentLine >= lines.length) {
+        block.innerHTML = html.join('')
+        return
+      }
 
-            const line = lines[currentLine];
-            const match = /^(\s*)(.*)/.exec(line);
-            const className = `userscript-console-${getType(line)}`;
-            const spaces = '&nbsp'.repeat(match[1].length);
-            const content = ansi_up.ansi_to_html(match[2]);
+      const line = lines[currentLine]
+      const match = /^(\s*)(.*)/.exec(line)
+      const className = `userscript-console-${getType(line)}`
+      const spaces = '&nbsp'.repeat(match[1].length)
+      const content = ansi_up.ansi_to_html(match[2])
 
-            html.push(`<div class="${className}">${spaces}${content}</div>`);
-        }
+      html.push(`<div class="${className}">${spaces}${content}</div>`)
+    }
 
-        setTimeout(parseChunk, 0);
-    })();
+    setTimeout(parseChunk, 0)
+  })()
 }
 
 function parseBlocks() {
-    query('pre.console-output:not(.parsed), #out pre:not(.parsed)').forEach(parseBlock);
+  query('pre.console-output:not(.parsed), #out pre:not(.parsed)').forEach(
+    parseBlock
+  )
 }
 
 function query(selector) {
-    return Array.from(document.querySelectorAll(selector));
+  return Array.from(document.querySelectorAll(selector))
 }
 
 css(`
@@ -118,7 +109,8 @@ css(`
     .userscript-console-success { color: #080 }
     .userscript-console-warning { color: #F0F }
     .userscript-console-error { color: #F00 }
-`);
+`)
 
-include('https://cdn.rawgit.com/drudru/ansi_up/32a3c2deb983579fe6149fba4938ce0f840d2afd/ansi_up.js')
-    .then(() => setInterval(parseBlocks, 200));
+include(
+  'https://cdn.rawgit.com/drudru/ansi_up/32a3c2deb983579fe6149fba4938ce0f840d2afd/ansi_up.js'
+).then(() => setInterval(parseBlocks, 200))
